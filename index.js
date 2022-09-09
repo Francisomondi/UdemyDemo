661cc3b21483278a0b09219e097e6f9c4d39976e
francis omondi <francisomondi17@gmail.com>
2022-09-09 21:49:17 +0300

    creating a schema and a model from the schema, saving items to mongo db

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/playground")
+    .then(() => {
+        console.log("connected to mongodb")
+    })
+    .catch(err => console.error("cound not connect to the db" + err))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    isPublished: Boolean
+
+})
+
+const Course = mongoose.model("Course", courseSchema)
+
+const createCourse = async () => {
+    const course = new Course({
+        name: "react tutors",
+        author: "Francis Omondi",
+        tags: ["react", "frontend"],
+        isPublished: true
+    })
+    const result = await course.save()
+    console.log(result)
+}
+
+createCourse()
