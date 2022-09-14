631df083499280573c018b6a30aed142e0438e10
francis omondi <francisomondi17@gmail.com>
2022-09-14 09:59:59 +0300

    result of a callback function

--- trial.js
+++ trial.js
@@ -1,5 +1,17 @@
 console.log("before")
-setTimeout(() => {
-    console.log("Getting a user from the datABASE....")
-}, 2000)
+getUser(1, function (user) {
+    console.log("user", user)
+})
 console.log("after")
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log("Getting a user from the datABASE....")
+
+        callback({
+            id: id,
+            gitHubUsername: "moash"
+        })
+
+    }, 2000)
+}
