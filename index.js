2d05996ed7865667de613f9419ff3f5e84f3f12f
francis omondi <francisomondi17@gmail.com>
2022-09-17 00:25:14 +0300

    updating data in mongodb

--- index.js
+++ index.js
@@ -39,12 +39,22 @@ const getCourses = async () => {
     //and
     const courses = await Course
         //.find({ author: "Unique mweni", isPublished: true })
-        .find()
-        .or([{
-            author: "Unique mweni"
-        }, {
-            isPublished: true
-        }])
+
+        //.find().or([{ author: "Unique mweni" }, {  isPublished: true }]).and([])
+
+        //starts with Unique
+        .find({
+            author: /^Unique/
+        })
+        //ends with omondi
+        .find({
+            author: /omondi$/i
+        })
+        //courses with author containing omo
+        .find({
+            author: /.*omo.*/i
+        })
+
         .limit(10)
         .sort({
             name: 1
@@ -57,4 +67,19 @@ const getCourses = async () => {
     console.log(courses)
 
 }
-getCourses()
+
+//updating courses
+const updateCourse = async (id) => {
+    const course = await Course.findById(id)
+    if (!course) return;
+
+    course.name = "Mstahiki mayor",
+        course.price = 13,
+        course.author = "walah bin walah",
+        course.tags = ["kiswahili", "kitukuzwe"],
+        course.isPublished = true
+
+    const result = await course.save()
+    console.log(result)
+}
+updateCourse("631b860a07c7cd0bba56a489")
