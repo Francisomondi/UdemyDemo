aaf1d601505029000b31f252c760c20fe28b33c0
francis omondi <francisomondi17@gmail.com>
2022-09-20 18:42:00 +0300

    removing nested documents using the unset property

--- embedding.js
+++ embedding.js
@@ -36,8 +36,8 @@ async function updateAuthor(courseId) {
   const course = await Course.updateOne({
     _id: courseId
   }, {
-    $set: {
-      'author.name': "Omondi francis"
+    $unset: {
+      'author': ""
     }
   })
 }
